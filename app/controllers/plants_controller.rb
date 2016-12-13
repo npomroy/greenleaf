03e1ff685fd19a8b2f68f143db1dfa0849ab14e1
Nick Pomroy <npomroy69@hotmail.com>
2016-12-13 06:28:13 +0000

    Added plant create action

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,4 +3,22 @@ class PlantsController < ApplicationController
     def new
         @plant = Plant.new
     end
+    
+    # POST to /users/:user_id/plants
+    def create
+       @plant = Plant.new( plant_params )
+       @plant.user_id = (current_user.id)
+       if @plant.save
+           flash[:success] = "Plant created"
+           redirect_to root_path
+       else
+           flash[:error] = "Plant creation failed"
+           render action: :new
+       end
+    end
+    
+    private
+        def plant_params
+           params.require(:plant).permit(:name, :user_id, :light_id, :plant_date, :last_turn, :growth_stage, :water_level, :description) 
+        end
 end
